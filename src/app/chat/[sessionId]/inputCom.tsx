0dc5279e23a6071a81ded2c24679865ea15616ac
Terminal017 <1404256812@qq.com>
2026-02-25 17:23:46 +0800

    修复多session状态更新问题

--- src/app/chat/[sessionId]/inputCom.tsx
+++ src/app/chat/[sessionId]/inputCom.tsx
@@ -1,11 +1,22 @@
 import { Button } from '@/components/ui/button'
 import { CircleArrowUp } from 'lucide-react'
 import { useChatStore } from '@/store/chat-store'
-import { useState } from 'react'
+import { use, useState } from 'react'
 import { useWorkerManager } from '@/store/ai-store'
 import { toast } from 'sonner'
 import * as Comlink from 'comlink'
 import { useParams } from 'next/navigation'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  SelectLabel,
+} from '@/components/ui/select'
+import { useLiveQuery } from 'dexie-react-hooks'
+import { getAllLibraries } from '@/services/libraryService'
 
 export default function InputCom() {
   const { sessionId } = useParams()
@@ -38,26 +49,51 @@ export default function InputCom() {
     setQuestion('')
 
     const prompt = question_msg
-    initStreaming()
+
+    //闭包传递sessionId
+    initStreaming(Number(sessionId))
     await modelWorker.api.generateStreaming(
       prompt,
       Comlink.proxy((token: string) => {
         // 【高频触发区】Zustand 接收字符，仅更新内存
-        appendChunk(token)
+        appendChunk(token, Number(sessionId))
       }),
     )
     finishStreaming(Number(sessionId))
   }
 
+  //获取知识库信息
+  const libraryList = useLiveQuery(() => getAllLibraries() ?? [], []) ?? []
+  const messageList = useChatStore((state) => state.messageList)
+  // 更新知识库状态
+  const currentLibraryId = useChatStore((state) => state.currentLibraryId)
+  const setLibrary = useChatStore((state) => state.setCurrentLibrary)
+
   return (
-    <div className="w-3/5 mx-auto my-8">
-      <div
-        className="flex flex-row items-center border shadow-sm
-          rounded-md p-1"
+    <div className="w-3/5 mx-auto mb-8 mt-4">
+      <Select
+        value={currentLibraryId}
+        onValueChange={setLibrary}
+        disabled={messageList.length > 0}
       >
+        <SelectTrigger className="w-48 mb-2">
+          <SelectValue placeholder="选择知识库" />
+        </SelectTrigger>
+        <SelectContent position="popper">
+          <SelectGroup>
+            <SelectLabel>知识库</SelectLabel>
+            <SelectItem value="none">无知识库</SelectItem>
+            {libraryList.map((lib) => (
+              <SelectItem key={lib.id} value={lib.id.toString()}>
+                {lib.name}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+      <div className="flex flex-row items-center border shadow-sm rounded-md p-1">
         <textarea
-          className="w-full resize-none overflow-hidden
-               px-3 py-2 focus:outline-none"
+          className="w-full resize-none overflow-hidden px-3 py-2 focus:outline-none"
           rows={1}
           value={question}
           onChange={(e) => {
--- src/store/chat-store.ts
+++ src/store/chat-store.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand'
 import { getMessageList, createMessage } from '@/services/messageService'
-import { getSessionItem } from '@/services/sessionService'
+import { getSessionItem, updateSession } from '@/services/sessionService'
 
 type MessageType = {
   role: 'user' | 'model'
@@ -8,15 +8,18 @@ type MessageType = {
   createAt: Date
 }
 
+//当前session状态
 type ChatStateType = {
   currentSessionId: number | null
-  currentLibraryId: number | null
-  messageList: MessageType[]
+  currentLibraryId: string | 'none' //更改为string，方便修改
+  messageList: MessageType[] //全局消息
+  backgroundStreams: { [sessionId: number]: string } //每个会话的流式输出缓存
   isStreaming: boolean
   setCurrentSession: (sessionId: number) => void
-  appendChunk: (chunk: string) => void
+  setCurrentLibrary: (libraryId: string | 'none') => void
+  appendChunk: (chunk: string, sessionId: number) => void
   addMessage: (message: MessageType) => void
-  initStreaming: () => void
+  initStreaming: (sessionId: number) => void
   finishStreaming: (sessionId: number) => void
 }
 
@@ -26,20 +29,22 @@ let rafId: number | null = null
 
 const useChatStore = create<ChatStateType>((set, get) => ({
   currentSessionId: null, //当前会话ID
-  currentLibraryId: null, //当前会话关联的知识库ID
+  currentLibraryId: 'none', //当前会话关联的知识库ID
   messageList: [], //当前会话的消息列表
+  backgroundStreams: {}, //会话ID到流式输出缓存的映射
   isStreaming: false, //是否正在接收消息流
-  //初始化新会话
+  //初始化新会话或已有对话
   setCurrentSession: async (sessionId) => {
     //先重置消息队列
     set({
       currentSessionId: sessionId,
+      currentLibraryId: 'none',
       messageList: [],
     })
 
     //读取会话信息和消息历史并更新
     const session = await getSessionItem(sessionId)
-    const libraryId = session?.libraryId || null
+    const libraryId = session?.libraryId?.toString() || 'none'
     const historyData = await getMessageList(sessionId)
     const history = historyData.map((msg) => ({
       role: msg.sender,
@@ -47,6 +52,18 @@ const useChatStore = create<ChatStateType>((set, get) => ({
       createAt: msg.createdAt,
     }))
     set({ currentLibraryId: libraryId, messageList: history })
+  },
+
+  //更新知识库
+  setCurrentLibrary: async (libraryId: string | 'none') => {
+    const session = get().currentSessionId
+    if (!session) {
+      return
+    }
+    set({ currentLibraryId: libraryId })
+    await updateSession(session, {
+      libraryId: libraryId === 'none' ? null : Number(libraryId),
+    })
   },
 
   //添加新消息到消息列表
@@ -66,7 +83,7 @@ const useChatStore = create<ChatStateType>((set, get) => ({
   },
 
   //初始化流式输出
-  initStreaming: () => {
+  initStreaming: (sessionId: number) => {
     // 重置 RAF 状态
     buffer = ''
     if (rafId) {
@@ -83,41 +100,68 @@ const useChatStore = create<ChatStateType>((set, get) => ({
     set((state) => ({
       messageList: [...state.messageList, placeholderMsg],
       isStreaming: true,
+      backgroundStreams: { ...state.backgroundStreams, [sessionId]: '' }, //初始化流式输出缓存
     }))
   },
   //同步消息状态
-  appendChunk: (chunk) => {
+  appendChunk: (chunk, sessionId) => {
     buffer += chunk
 
+    //更新流式输出缓存
     if (!rafId) {
       rafId = requestAnimationFrame(() => {
+        const { currentSessionId, messageList } = get()
         set((state) => {
-          const messages = state.messageList
-          const lastMsg = messages[messages.length - 1]
+          const newStream = {
+            ...state.backgroundStreams,
+            [sessionId]: (state.backgroundStreams[sessionId] || '') + buffer, //累加流
+          }
 
-          if (lastMsg?.role === 'model') {
-            return {
-              messageList: [
-                ...messages.slice(0, -1),
-                { ...lastMsg, content: lastMsg.content + buffer },
-              ],
+          // 如果是当前会话,同步更新 messageList
+          if (currentSessionId === sessionId) {
+            const lastMsg = messageList[messageList.length - 1]
+
+            // 如果最后一条是 user 消息,追加新的 model 消息
+            if (lastMsg?.role === 'user') {
+              return {
+                backgroundStreams: newStream,
+                messageList: [
+                  ...messageList,
+                  {
+                    role: 'model' as const,
+                    content: newStream[sessionId],
+                    createAt: new Date(),
+                  },
+                ],
+              }
+            }
+
+            // 如果最后一条是 model 消息,更新其内容
+            if (lastMsg?.role === 'model') {
+              const msgs = [...messageList]
+              msgs[msgs.length - 1] = {
+                ...lastMsg,
+                content: newStream[sessionId],
+              }
+              return {
+                backgroundStreams: newStream,
+                messageList: msgs,
+              }
             }
           }
-          return state
+
+          //如果不是当前会话,只在后台更新流式输出缓存
+          return {
+            backgroundStreams: newStream,
+          }
         })
+
         buffer = ''
         rafId = null
       })
     }
-    // set((state) => {
-    //   const newMessages = [...state.messageList]
-    //   const lastMsg = newMessages[newMessages.length - 1]
-    //   if (lastMsg && lastMsg.role === 'model') {
-    //     lastMsg.content += chunk
-    //   }
-    //   return { messageList: newMessages }
-    // })
   },
+
   //结束流式传输
   finishStreaming: async (sessionId: number) => {
     const { messageList, isStreaming } = get()
@@ -131,29 +175,28 @@ const useChatStore = create<ChatStateType>((set, get) => ({
     }
     if (buffer) {
       set((state) => {
-        const messages = state.messageList
-        const lastMsg = messages[messages.length - 1]
-        if (lastMsg?.role === 'model') {
-          return {
-            messageList: [
-              ...messages.slice(0, -1),
-              { ...lastMsg, content: lastMsg.content + buffer },
-            ],
-          }
+        return {
+          backgroundStreams: {
+            ...state.backgroundStreams,
+            [sessionId]: (state.backgroundStreams[sessionId] || '') + buffer,
+          },
         }
-        return state
       })
       buffer = ''
     }
 
-    const lastMsg = messageList[messageList.length - 1]
+    const lastMsg = {
+      role: 'model',
+      content: get().backgroundStreams[sessionId] || '',
+      createAt: new Date(),
+    }
     //写入数据库
     await createMessage({
       sessionId: sessionId,
       sender: 'model',
       content: lastMsg.content,
     })
-    set({ isStreaming: false })
+    set({ isStreaming: false, backgroundStreams: {} })
   },
 }))
 
